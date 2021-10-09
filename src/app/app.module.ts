3d237a18ec7ffba72eee8df71d9e64a2ab8266d1
Ricardo Belfort <ricardo.belfort@opah.com.br>
2021-10-09 11:25:07 -0400

    Creating a shared material module for easy reuse

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/subsidiaries/subsidiaries.module.ts
+++ src/app/pages/subsidiaries/subsidiaries.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { SubsidiariesRoutingModule } from './subsidiaries-routing.module';
 import { SubsidiariesListComponent } from './subsidiaries-list/subsidiaries-list.component';
 import { SubsidiariesFormComponent } from './subsidiaries-form/subsidiaries-form.component';
-
+import { AppMaterialModule } from 'src/app/shared/app-material/app-material.module';
 
 @NgModule({
-  declarations: [
-    SubsidiariesListComponent,
-    SubsidiariesFormComponent
-  ],
-  imports: [
-    CommonModule,
-    SubsidiariesRoutingModule
-  ]
+  declarations: [SubsidiariesListComponent, SubsidiariesFormComponent],
+  imports: [CommonModule, SubsidiariesRoutingModule, AppMaterialModule],
 })
-export class SubsidiariesModule { }
+export class SubsidiariesModule {}
